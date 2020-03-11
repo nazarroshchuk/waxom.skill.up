994ec2f7d7712d0216ab94a5ee5968c7905d89ec
nazarroshchuk <nazarroshchuk@gmail.com>
2020-03-11 15:41:46 +0200

    add filter add cctegory in filter add videopresent

---
+++ js/filter.js
@@ -0,0 +1,62 @@
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterFig");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+// Add active class to the current control button (play video)
+
+let playBtnVideo = document.getElementById('play-btn');
+// playBtnVideo.addEventListener("click", addClassForPlay());
+
+function addClassForPlay() {
+    let element, name, arr;
+    element = document.getElementById('video-for-play');
+    name = "play-video";
+    arr = element.className.split(" ");
+    if (arr.indexOf(name) === -1) {
+        element.className += " " + name;
+    }
+}
+
