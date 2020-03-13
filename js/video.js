6f0b7175cf8aa1a949c71ff2315d5fc182d1aa27
nazarroshchuk <nazarroshchuk@gmail.com>
2020-03-13 19:40:48 +0200

    fix projects-galery flex & video js

--- js/video.js
+++ js/video.js
@@ -1,5 +1,4 @@
 
-
 // Add active class to the current control button (play video)
 
 let playBtnVideo = document.getElementById('play-btn');
@@ -8,20 +7,14 @@ playBtnVideo.addEventListener("click", addClassForPlay);
 function addClassForPlay() {
     let element, name, arr;
     element = document.getElementById('ytplayer');
-    name = "play-video";
-    arr = element.className.split(" ");
-    if (arr.indexOf(name) === -1) {
-        element.className += name;
-    }
+    element.classList.add('play-video');
+
     // Load the IFrame Player API code asynchronously.
     var tag = document.createElement('script');
     tag.src = "https://www.youtube.com/player_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 };
-
-
-
 
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
