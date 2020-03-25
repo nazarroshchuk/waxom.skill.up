5207570069cd31b00d6c9e5d1e48a40323beaeee
nazarroshchuk <nazarroshchuk@gmail.com>
2020-03-25 16:33:45 +0200

    btn up

--- js/events.js
+++ js/events.js
@@ -1,3 +1,4 @@
+// Menu fixed scroll
 var menuFixed = document.querySelector('.header-up-active');
 document.addEventListener('scroll', function () {
     if (document.documentElement.scrollTop > 50) {
@@ -6,6 +7,25 @@ document.addEventListener('scroll', function () {
         menuFixed.classList.remove('active');
     }
 });
+//Get the button
+var myButton = document.getElementById("myBtnUp");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener('scroll', function () { scrollFunction() });
+window.addEventListener('click', function () { topFunction() });
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 
